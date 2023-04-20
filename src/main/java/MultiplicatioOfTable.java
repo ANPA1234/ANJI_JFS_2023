63b5f00f520fa15ac3796169732c603f9fa25ccc
ANPA1234 <131011668+ANPA1234@users.noreply.github.com>
2023-04-20 11:04:27 -0400

    Extra log message and multiplication of tableadded. (#1)

---
+++ src/main/java/MultiplicatioOfTable.java
@@ -0,0 +1,8 @@
+public class MultiplicatioOfTable {
+    public static void main(String[] args) {
+        int num = 19;
+        for(int i=1;i<=10;i++){
+            System.out.println(String.format("%s * %s = %s",num,i,num*i));
+        }
+    }
+}
