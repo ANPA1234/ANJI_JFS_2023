4bf99056a08ea19a87fc817f76a127b9ae7d5b24
ANPA1234 <131011668+ANPA1234@users.noreply.github.com>
2023-04-20 11:14:05 -0400

    changed username as admin and double quotes  message

--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -1,9 +1,9 @@
 public class Greetings {
     public static void main(String[] args) {
-        String username = "user";
+        String username = "admin";
         if (args.length>0){
             username = args[0];
         }
-        System.out.println("Hi "+username+". \n\twelcome to java world");
+        System.out.println("Hi "+username+". \n\t"welcome to java world"");
     }
 }
