52b1edac6dff0fac654dae0d139a1ecc8fd1da94
ANPA1234 <anjaneyulude159@gmail.com>
2023-04-20 11:34:28 -0400

    LWL001-fixed userinput and change 10 t 20 times

--- src/main/java/MultiplicatioOfTable.java
+++ src/main/java/MultiplicatioOfTable.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class MultiplicatioOfTable {
     public static void main(String[] args) {
-        int num = 19;
-        for(int i=1;i<=10;i++){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Num:");
+        int num = sc.nextInt();
+        for(int i=1;i<=20;i++){
             System.out.println(String.format("%s * %s = %s",num,i,num*i));
         }
     }
